d05099bda0fe34c92e330a44cd3eb66e48a84f51
Alexandr <ak4raulov@yandex.ru>
2021-08-04 18:47:38 +0300

    sections skills portfolio

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@
 const hamburger = document.querySelector('.hamburger'),
 	menu = document.querySelector('.menu'),
 	closeMenu = document.querySelector('.menu__close'),
-	menuLink = document.querySelector('.menu__link'),
-	promoLink = document.querySelector('.promo__link');
+	menuLink = document.querySelector('.menu__link');
+
 
 
 hamburger.addEventListener('click', () => {
@@ -14,4 +14,11 @@ closeMenu.addEventListener('click', () => {
 });
 menuLink.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__rating-interest'),
+	lines = document.querySelectorAll('.skills__rating-line span');
+
+counters.forEach((item, i) => {
+	lines[i].style.width = item.innerHTML;
 });
